8231aff06f1ae6dfc617f605bf374ed4a29c5c3f
M-5moulin <mtthieumoulin@gmail.com>
2026-03-03 11:21:24 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -16,7 +16,7 @@ import {
 } from 'lucide-react';
 
 // Clé API vide par défaut (sera remplie par Vercel Environment Variables ou le champ manuel)
-const apiKey = "AIzaSyCLt10LXlH1wZYd9x3sO3kaS7BXTLIuaKM"; 
+const apiKey = "AIzaSyDES1Mk0GBikszdRH2gTKPpITpL1C5YPqs"; 
 
 const REANIMATION_PROMPT = `
 Tu es un médecin interne en réanimation. Ta tâche est de rédiger un Compte Rendu de Sortie d'Hospitalisation (CRH) formel.
